e9ac5cb9fc30feca1f3333bcb934b4143f5eec4d
PankajkandpalL <pankajkandpal5602@gmail.com>
2023-10-08 22:49:26 +0530

    feat: added errorhandlers and implemented cors

--- server/src/index.js
+++ server/src/index.js
@@ -4,13 +4,21 @@ const helmet = require('helmet');
 const session = require('express-session');
 const { connectToDb } = require('./config/dbConfig');
 const { health } = require('./heathCheckups/healthCheckups');
+const { authRouter } = require('./features/auth/auth.routes');
+const cors = require("cors");
+const allowedOrigins = require('./config/cors/allowedOrigins');
+const { generalErrorHandler } = require('./errorHandling/generalErrorHandler');
+const { authErrorHandler } = require('./errorHandling/authErrorHandler');
 
 const app = express();
 app.use(express.json({ limit : "1mb" }))
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(cors(allowedOrigins));
 
-app.use("/v1/health", health)
+app.use("/v1/health", health);
+app.use("/v1/auth", authRouter);
+app.use(authErrorHandler, generalErrorHandler)
 
 app.listen(process.env.PORT, async()=>{
     try{
